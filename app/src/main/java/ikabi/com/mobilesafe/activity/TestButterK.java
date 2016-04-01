39aa86c8c6b44d2e8d64b23eee99ba8c4ff8f405
Heinilup <379869869@qq.com>
2016-04-01 22:44:42 +0800

    add butterknife  to this project
    add butterknife

---
+++ app/src/main/java/ikabi/com/mobilesafe/activity/TestButterK.java
@@ -0,0 +1,19 @@
+package ikabi.com.mobilesafe.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ikabi.com.mobilesafe.R;
+
+/**
+ * @ Author: Shuangjun Zou(Rob)
+ * @ Email: seolop@gmail.com
+ * @ 2016-04-01 0001
+ */
+public class TestButterK extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smarthome);
+    }
+}
