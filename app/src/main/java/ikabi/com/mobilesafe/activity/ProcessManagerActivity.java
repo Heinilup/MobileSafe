ac40289b50a50dbce03107614fb452264236e893
Heinilup <379869869@qq.com>
2016-04-03 22:22:58 +0800

    modify the process manager
    add list view adapter

--- app/src/main/java/ikabi/com/mobilesafe/activity/ProcessManagerActivity.java
+++ app/src/main/java/ikabi/com/mobilesafe/activity/ProcessManagerActivity.java
@@ -3,8 +3,17 @@ package ikabi.com.mobilesafe.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.format.Formatter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import ikabi.com.mobilesafe.R;
+import ikabi.com.mobilesafe.bean.ProcessInfo;
 import ikabi.com.mobilesafe.provider.ProcessProvider;
 import ikabi.com.mobilesafe.service.AutoCleanService;
 import ikabi.com.mobilesafe.utils.ServiceStateUtils;
@@ -16,17 +25,19 @@ import ikabi.com.mobilesafe.view.ProgressDesView;
  * @ Data:16/2/21
  */
 public class ProcessManagerActivity extends Activity {
-    private ProgressDesView mPdvProcess;
-    private ProgressDesView mPdvMemory;
+    @Bind(R.id.pm_pdv_process)
+    ProgressDesView mPdvProcess;
+    @Bind(R.id.pm_Pdv_memory)
+    ProgressDesView mPdvMemory;
+    @Bind(R.id.pm_list_view)
+    ListView mListView;
+    private List<ProcessInfo> mDates;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_process_manager);
-
-
-        //init view
-        mPdvProcess = (ProgressDesView) findViewById(R.id.pm_pdv_process);
-        mPdvMemory = (ProgressDesView) findViewById(R.id.pm_Pdv_memory);
+        ButterKnife.bind(this);
 
         //progress
         int runningProcessCount = ProcessProvider.getRunningProcessCount(this);
@@ -34,7 +45,7 @@ public class ProcessManagerActivity extends Activity {
         mPdvProcess.setDesTitle("进程数:");
         mPdvProcess.setDesLeftTitle("正在运行" + runningProcessCount + "个");
         mPdvProcess.setDesRightTitle("可有进程" + totalProcessCount + "个");
-        mPdvProcess.setDesProgress((int) (runningProcessCount * 100f / totalProcessCount +0.5f));
+        mPdvProcess.setDesProgress((int) (runningProcessCount * 100f / totalProcessCount + 0.5f));
 
         //Memory
         long freeMemory = ProcessProvider.getFreeMemory(this);
@@ -43,11 +54,45 @@ public class ProcessManagerActivity extends Activity {
         mPdvMemory.setDesTitle("内存:");
         mPdvMemory.setDesLeftTitle("占用内存:" + Formatter.formatFileSize(this, usedMemory));
         mPdvMemory.setDesRightTitle("可用内存:" + Formatter.formatFileSize(this, freeMemory));
-        mPdvMemory.setDesProgress((int) (usedMemory * 100f / totalMemory +0.5f));
+        mPdvMemory.setDesProgress((int) (usedMemory * 100f / totalMemory + 0.5f));
+
+        //给ListView加载数据
+        mListView.setAdapter(null);
 
 
     }
+    private class ProcessAdapter extends BaseAdapter{
 
+        @Override
+        public int getCount() {
+            if (mDates != null){
+                return mDates.size();
+            }
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            if (mDates != null){
+                return mDates.get(i);
+            }
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, android.view.View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+
+    private class ViewHolder{
+        
+    }
     @Override
     protected void onStart() {
         super.onStart();
